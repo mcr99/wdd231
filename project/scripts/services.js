8280449407ccd40d48b163d51c3c4bff70bd87a0
mcr99 <mcanastuj1999@gmail.com>
2024-11-01 02:46:45 -0600

    adding last part

--- project/scripts/services.js
+++ project/scripts/services.js
@@ -14,6 +14,45 @@ const toggleContainer = document.querySelector('.toggle');
 
 // main
 
+async function fetchMembers() {
+    try {
+        const response = await fetch('data/services.json');
+        if (!response.ok) {
+            throw new Error(`Network response was not ok, status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        const services = data.services;
+        const actCont = document.querySelector('.activities');
+
+        if (actCont) {
+            actCont.innerHTML = '';
+
+           const selectedActivity = [];
+           for (let i = 0; i < 4; i++) {
+               const randomIndex = Math.floor(Math.random() * services.length);
+                selectedActivity.push(services[randomIndex]);
+            }
+
+            selectedActivity.forEach(services => {
+                const actCard = document.createElement('div');
+                actCard.classList.add('activity-card');
+                actCard.innerHTML = `
+                    <img src="${services.image}" alt="${services.activity} logo" width="50%" height="50%" loading="lazy"> 
+                    <h2>${services.details}</h2>
+                `;
+                actCont.appendChild(actCard);
+            });
+        } else {
+            console.error('Error: .activities container not found in fetchMembers');
+        }
+    } catch (error) {
+        console.error("Error fetching activities data:", error);
+    }
+}
+
+fetchMembers();
+
 // footer
 
 const yearSpan = document.querySelector('.year');
