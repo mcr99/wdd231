0581d241698a7bcad44f22803f560e08b4753a99
mcr99 <mcanastuj1999@gmail.com>
2024-11-01 01:32:27 -0600

    adding join us

--- project/scripts/joinus.js
+++ project/scripts/joinus.js
@@ -14,6 +14,40 @@ const toggleContainer = document.querySelector('.toggle');
 
 // main
 
+const activities = [
+    { activity: "Grooming" },
+    { activity: "Park Activities" },
+];
+
+const act = document.getElementById("activity");
+
+activities.forEach(activity => {
+    const option = document.createElement("option");
+    option.textContent = activity.activity;
+    act.appendChild(option);
+});
+
+const openButton1 = document.querySelector(".openButton1");
+const openButton2 = document.querySelector(".openButton2");
+const dialogBox = document.querySelector("#dialogBox");
+const closeButton = document.querySelector("#closeButton");
+const dialogBoxText = document.querySelector("#dialogBox div");
+
+
+openButton1.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxText.innerHTML = "The Grooming option will include haircut, nails and a shower. For just $20.00"
+});
+
+openButton2.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxText.innerHTML = "Park activities are available just for $5.00 per dog"
+});
+
+closeButton.addEventListener("click", () => {
+    dialogBox.close();
+});
+
 // footer
 
 const yearSpan = document.querySelector('.year');
